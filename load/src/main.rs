76650d9613553c722c72e74722fde3e12d64e7cd
Harald Hoyer <harald@profian.com>
2021-12-01 13:35:08 +0100

    Set per-package-target and forced-target
    Signed-off-by: Harald Hoyer <harald@profian.com>

--- load/src/main.rs
+++ load/src/main.rs
@@ -1,15 +1,23 @@
+#[cfg(features = "bindeps")]
 pub fn shim() -> &'static [u8] {
     include_bytes!(env!("CARGO_BIN_FILE_SHIM"))
 }
 
+#[cfg(features = "bindeps")]
 pub fn code() -> &'static [u8] {
     include_bytes!(env!("CARGO_BIN_FILE_CODE"))
 }
 
+#[cfg(features = "bindeps")]
 fn main() {
     let shim = shim();
     let code = code();
     println!("shim {}", env!("CARGO_BIN_FILE_SHIM"));
     println!("code {}", env!("CARGO_BIN_FILE_CODE"));
     println!("shim.len() = {}, code.len() = {}", shim.len(), code.len());
+}
+
+#[cfg(not(features = "bindeps"))]
+fn main() {
+    println!("Hello World!");
 }
